d15ac3e88e6da6d37a1eb7b8af1f93b07397c869
Dragan Iulian <iulianalexandrudragan@gmail.com>
2025-04-05 12:37:27 +0300

    feat: add Emotion cache

---
+++ src/createEmotionServer.ts
@@ -0,0 +1,8 @@
+// src/createEmotionServer.ts
+import createEmotionCache from "./createEmotionCache";
+import createEmotionServer from "@emotion/server/create-instance";
+
+const cache = createEmotionCache();
+const { extractCriticalToChunks, constructStyleTagsFromChunks } = createEmotionServer(cache);
+
+export { extractCriticalToChunks, constructStyleTagsFromChunks, cache };
