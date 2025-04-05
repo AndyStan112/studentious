f5ab159888dfae0d12ec323205661b617afd5162
Dragan Iulian <iulianalexandrudragan@gmail.com>
2025-04-05 13:54:57 +0300

    fix: remove cache

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,11 @@
 // app/layout.tsx
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
 import theme from "../theme";
-import { CacheProvider } from "@emotion/react";
-import createEmotionCache from "../createEmotionCache";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -23,21 +22,16 @@ export const metadata: Metadata = {
     description: "A platform for group learning sessions.",
 };
 
-// Create a client-side emotion cache instance
-const clientSideEmotionCache = createEmotionCache();
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
                 <ClerkProvider>
                     {/* Wrap with CacheProvider to ensure consistent class names */}
-                    <CacheProvider value={clientSideEmotionCache}>
-                        <ThemeProvider theme={theme}>
-                            <CssBaseline />
-                            {children}
-                        </ThemeProvider>
-                    </CacheProvider>
+                    <ThemeProvider theme={theme}>
+                        <CssBaseline />
+                        {children}
+                    </ThemeProvider>
                 </ClerkProvider>
             </body>
         </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,19 @@
-import Image from "next/image";
-import {
+"use client";
 
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs'
+import Image from "next/image";
+import { SignInButton, SignUpButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 export default function Home() {
-  return (
-    <div className="">
-    <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
-              <SignInButton mode="modal"/>
-              <SignUpButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
-    </div>
-  );
+    return (
+        <div className="">
+            <header className="flex justify-end items-center p-4 gap-4 h-16">
+                <SignedOut>
+                    <SignInButton mode="modal" />
+                    <SignUpButton />
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+            </header>
+        </div>
+    );
 }
